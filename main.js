250eee9fb80a6218a059222deb0c49f4752f725b
Tunar <96535500+Tunar1994@users.noreply.github.com>
2022-07-26 01:35:58 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#icon').click(function(){
+        $('ul').toggleClass('show')
+    })
+})
+
+$(document).ready(function(){
+    $('#icon1').click(function(){
+        $('ul').toggleClass('hidden')
+    })
+})
+
+
+
